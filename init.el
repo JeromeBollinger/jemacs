b9c44871ab8000c0f0b0398695df6b01d87da177
JeromeBollinger <jerome.f.bollinger@gmail.com>
2024-02-21 07:21:25 +0100

    chore: add neotree

--- init.el
+++ init.el
@@ -179,6 +179,25 @@
 (use-package yaml-mode
   :ensure t)
 
+(use-package neotree
+  :ensure t)
+
+;; NeoTree can be opened (toggled) at projectile project root
+(defun neotree-project-dir ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (neotree-toggle)
+      (if project-dir
+          (if (neo-global--window-exists-p)
+              (progn
+                (neotree-dir project-dir)
+                (neotree-find file-name)))
+        (message "Could not find git project root."))))
+
+;; need another one for python stuff, since this gets re-bound
+(global-set-key (kbd "C-b") 'neotree-project-dir)
 
 ;; Run sudo pamac install dotnet-sdk
 ;; Clone git@github.com:christiaan-janssen/bicep-mode.git
